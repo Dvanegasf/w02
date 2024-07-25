0c43f3bff0e3b0aa5d5a2ee4964b03f8544c633e
David Vanegas <dvanegasf2016@gmail.com>
2024-07-24 23:12:06 -0500

    update

---
+++ src/controllers/cars.controllers.js
@@ -0,0 +1,46 @@
+const catchError = require('../utils/catchError');
+const Car = require('../models/Cars');
+
+
+const getAll = catchError(async(req, res) => {
+    const car = await Car.findAll()
+    return res.json(car)
+
+});
+const create = catchError(async (req, res) => {
+    const result = await Car.create(req.body)
+    return res.status(201).json(result)
+});
+
+
+const getOne = catchError(async (req, res) => {
+    const { id } = req.params
+    const result = await Car.findByPk(id)
+    if (!result) return res.status(404).json('user notfound')
+    return res.json(result)
+});
+
+const destroy = catchError(async (req, res) => {
+    const { id } = req.params
+    const result = await Car.destroy({ where: { id }})
+    if (!result) return res.status(404).json('user notfound')
+    return res.sendStatus(204);
+})
+
+const update  = catchError(async (req, res) => {
+    const { id } = req.params
+    const car = await Car.update(
+        req.body,
+        { where: { id }, returing: true }
+        )
+    if  (car[0] === 0) return res.sendStatus(404)
+ console.log(car)
+    return res.json(car)
+    })
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    destroy,
+    update
+};
